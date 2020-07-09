75a454d98ddabec32bdac2b909fbafe14d2aeb4d
mlochead <mlochead@gmail.com>
2020-07-09 17:31:49 -0600

    Updated instance

--- example3/example3.tf
+++ example3/example3.tf
@@ -1,1 +1,21 @@
-# example of using outputs to get ip address of EIP
+#example of deploying multiple resources with an implicit dependency
+
+provider "aws" {
+  region     = "us-east-1"
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "tfex2578"
+  acl    = "private"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-2757f631"
+  instance_type = "t2.small"
+  depends_on = [aws_s3_bucket.example]
+}
+
+resource "aws_eip" "ip" {
+    vpc = true
+    instance = aws_instance.example.id
+}
