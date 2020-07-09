f4459e311b4fc5a888dc2c49bcc87574cacb30f5
Michael Lochead <michael@joyent.com>
2020-07-09 17:19:01 -0600

    Update example2.tf

--- example2/example2.tf
+++ example2/example2.tf
@@ -4,14 +4,11 @@ provider "aws" {
   region     = "us-east-1"
 }
 
-<<<<<<< HEAD:example2/example2.tf
 resource "aws_s3_bucket" "example" {
   bucket = "tfex2578"
   acl    = "private"
 }
 
-=======
->>>>>>> 3ca3c048f321c5d38eb96767b0c4eabd2bf037df:example2/example2
 resource "aws_instance" "example" {
   ami           = "ami-2757f631"
   instance_type = "t2.micro"
